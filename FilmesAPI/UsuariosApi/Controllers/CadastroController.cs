9f2d957d50d1a672f10a1bfb68d31e2f3ee55ee5
Thiago Rocha <ThIaGo_afr@hotmail.com>
2022-05-11 20:44:34 -0300

    Criando projeto para Login/Logout

---
+++ FilmesAPI/UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,27 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Service;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CadastroController : Controller
+    {
+        private CadastroService _cadastroService;
+
+        public CadastroController(CadastroService cadastroService)
+        {
+            _cadastroService = cadastroService;
+        }
+
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            Result resultado = _cadastroService.CadastraUsuario(createDto);
+            if (resultado.IsFailed) return StatusCode(500, "Houve um erro no servidor");
+            return Ok();
+        }
+    }
+}
---
+++ FilmesAPI/UsuariosApi/Service/LogoutService.cs
@@ -0,0 +1,23 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace UsuariosApi.Service
+{
+    public class LogoutService
+    {
+        private SignInManager<IdentityUser<int>> _signInManager;
+
+        public LogoutService(SignInManager<IdentityUser<int>> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public Result DeslogaUsuario()
+        {
+            var resultadoIdendity = _signInManager.SignOutAsync();
+            if(resultadoIdendity.IsCompletedSuccessfully) return Result.Ok();
+            return Result.Fail("Logout falhou");
+        }
+    }
+}
