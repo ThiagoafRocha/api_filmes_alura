10aa1574ecc9fe99694eeb1557754c19cca9ec58
Thiago Rocha <ThIaGo_afr@hotmail.com>
2022-05-24 20:14:34 -0300

    Incluindo redefinição de senha

---
+++ FilmesAPI/UsuariosApi/Data/Request/EfetuaResetRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosApi.Data.Request
+{
+    public class EfetuaResetRequest
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string Token { get; set; }
+    }
+}
